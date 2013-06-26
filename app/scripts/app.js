ae7a07a4b4d0c754eeafbd6eccaa6ea6ca0b152a
jiverson <jiverson222@gmail.com>
2013-06-26 08:48:29 -0700

    Initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('mfApiExampleApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mfApiExampleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'SDK',
+      'Sandbox',
+      'Other'
+    ];
+  });
