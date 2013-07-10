722c7813cc2ec0b0ccdac61d3b2a25c1effeac15
jiverson <jiverson222@gmail.com>
2013-07-10 12:26:00 -0700

    Added in docs section for Larsen to start filling out.

--- app/sandbox/sandbox.js
+++ app/sandbox/sandbox.js
@@ -8,7 +8,8 @@ angular.module('mfApiExampleApp').controller('SandboxCtrl', function($scope, $ht
 
 	$scope.apiMethods = [
 		{ name: 'Authorize user', type: 'GET', url: '/api/:version/auth',
-            params: {'id':'', 'courses':'', 'email':'', 'username':''} },
+            params: {'id':'', 'courses':'', 'email':'', 'username':''},
+            doc: 'docs/some-api.html' },
 		{ name: 'Add users', type: 'POST', url: '/api/:version/user',
             data: {'users': [{'firstName':'',lastName:'', 'email':''}], 'requiredCourseIds': [], 'courseIds': [], 'seriesIds': [], 'groupIds': [], 'clientDatestamp': ''} },
 		{ name: 'Archive user', type: 'POST', url: '/api/:version/user/:userId/archive' },
