0b33aa3a971f2ab286748f20bda3529b519bb62c
Cameron Gray <cameron.gray@gmail.com>
2013-07-23 17:02:36 -0700

    removed hardcoded api-key

--- app/sandbox/sandbox.js
+++ app/sandbox/sandbox.js
@@ -1,26 +1,28 @@
 'use strict';
 
-angular.module('mfApiExampleApp').controller('SandboxCtrl', function($scope, $http, $filter) {
+angular.module('mfApiExampleApp').controller('SandboxCtrl', function ($scope, $http, $filter) {
 
-    var baseUrl = 'http://api.mftdev.com';
-    var formatFilter = $filter('format');
-    var tokenRegEx = /(\:)(\w+)(\/{0,1})/gi;
+	var baseUrl = 'http://api.mftdev.com';
+	var formatFilter = $filter('format');
+	var tokenRegEx = /(\:)(\w+)(\/{0,1})/gi;
 
 	var today = $filter('date')(new Date(), 'yyyy-MM-dd');
-	
+
 	$scope.apiMethods = [
 		// Users
 		{ name: 'Authorize User', type: 'GET', url: '/api/:version/auth',
-			qs: [ 'id','username','email','courses' ],
-            doc: 'docs/auth-api.html', header: "users" },
+			qs: [ 'id', 'username', 'email', 'courses' ],
+			doc: 'docs/auth-api.html', header: "users" },
 		{ name: 'Get User Info', type: 'GET', url: '/api/:version/user/:userId',
-			qs: [ '_type','_status' ],
+			qs: [ '_type', '_status' ],
 			doc: 'docs/get-user-api.html', header: "users" },
 		{ name: 'Add Users', type: 'POST', url: '/api/:version/user',
-            data: {'users': [{'firstName':'',lastName:'', 'email':''}], 'requiredCourseIds': [], 'courseIds': [], 'seriesIds': [], 'groupIds': [], 'clientDatestamp': today, 'batchId' : '1'},
+			data: {'users': [
+				{'firstName': '', lastName: '', 'email': ''}
+			], 'requiredCourseIds': [], 'courseIds': [], 'seriesIds': [], 'groupIds': [], 'clientDatestamp': today, 'batchId': '1'},
 			doc: 'docs/add-user-api.html', header: "users"},
 		{ name: 'Edit User', type: 'POST', url: '/api/:version/user/:userId',
-			data: {'firstName':'',lastName:'', 'email':''},
+			data: {'firstName': '', lastName: '', 'email': ''},
 			doc: 'docs/edit-user-api.html', header: "users"},
 		{ name: 'Archive User', type: 'POST', url: '/api/:version/user/:userId/archive',
 			doc: 'docs/archive-user-api.html', header: "users"},
@@ -73,142 +75,152 @@ angular.module('mfApiExampleApp').controller('SandboxCtrl', function($scope, $ht
 			doc: 'docs/invite-users-series-api.html', header: "series" }
 	];
 
-	$scope.methodGroups = _.groupBy($scope.apiMethods, function(method) { return method.header; });
+	$scope.methodGroups = _.groupBy($scope.apiMethods, function (method) {
+		return method.header;
+	});
 
 	$scope.currentMethod = null;
-    $scope.repeater = {};
+	$scope.repeater = {};
 
 	$scope.viewModel = {
-        keySaved: false
+		keySaved: false
 	};
 
-    $scope.resultInfo = {};
+	$scope.resultInfo = {};
 
-    $scope.apiModel = {
-        apiKey: '',
-        version: 'v2'
-    };
+	$scope.apiModel = {
+		apiKey: '',
+		version: 'v2'
+	};
 
 	$scope.queryString = "";
-	
-    function initialize() {
-        _.each($scope.apiMethods, function(item) {
-            item.usageUrl = item.url;
-            item.url = item.url.replace(':version', $scope.apiModel.version);
-            item.tokenNames = getMatches(item.url, tokenRegEx, 2)
-        });
 
-        // for testing only uncomment out this section and add your api key -- don't commit
-        $scope.apiModel.apiKey = '32bbb158dbd24c3f853aed577b415dc0';
-        $scope.enterApiInfo();
-        $scope.selectMethod($scope.apiMethods[1]);
-    }
+	function initialize() {
+		_.each($scope.apiMethods, function (item) {
+			item.usageUrl = item.url;
+			item.url = item.url.replace(':version', $scope.apiModel.version);
+			item.tokenNames = getMatches(item.url, tokenRegEx, 2)
+		});
 
-    $scope.enterApiInfo = function(type) {
-        if(type == 'edit') {
-            $scope.viewModel.keySaved = false;
-            return;
-        }
-        if(!$scope.apiModel.apiKey) return;
+		// for testing only uncomment out this section and add your api key -- don't commit
+		$scope.apiModel.apiKey = '';
+		$scope.enterApiInfo();
+		$scope.selectMethod($scope.apiMethods[1]);
+	}
 
-        $scope.viewModel.keySaved = true;
-        $http.defaults.headers.common['x-mindflash-Apikey'] = $scope.apiModel.apiKey;
-    };
+	$scope.enterApiInfo = function (type) {
+		if (type == 'edit') {
+			$scope.viewModel.keySaved = false;
+			return;
+		}
+		if (!$scope.apiModel.apiKey) return;
 
-	$scope.updateQuery = function() {
+		$scope.viewModel.keySaved = true;
+		$http.defaults.headers.common['x-mindflash-Apikey'] = $scope.apiModel.apiKey;
+	};
+
+	$scope.updateQuery = function () {
 		var p = getParams();
-		if(p) {
+		if (p) {
 			var parts = [];
-			_.each(p, function(v, k) { parts.push(k + "=" + encodeURIComponent(v));});
+			_.each(p, function (v, k) {
+				parts.push(k + "=" + encodeURIComponent(v));
+			});
 			$scope.queryString = "?" + parts.join("&");
-		}  else {
+		} else {
 			$scope.queryString = "";
 		}
 	}
-	
-	$scope.selectMethod = function(method) {
+
+	$scope.selectMethod = function (method) {
 		$scope.resultInfo = {};
 		$scope.currentMethod = method;
 
 		var queryParams = {};
-		_.each($scope.currentMethod.qs, function(p) {queryParams[p] = "";});
-		
-		var tokens = {};
-		_.each($scope.currentMethod.tokenNames, function(p) {tokens[p] = "";});
-		
-        $scope.currentConfig = {
-            tokens: tokens,
-            params: queryParams,
-            data: angular.toJson(angular.copy($scope.currentMethod.data), true) || {}
-        };
+		_.each($scope.currentMethod.qs, function (p) {
+			queryParams[p] = "";
+		});
 
-        // use repeater so that there is no double binding in key,value
-        $scope.repeater = {
-            tokenNames: angular.copy($scope.currentMethod.tokenNames),
-	        qs: angular.copy($scope.currentMethod.qs),
-            data: angular.copy($scope.currentMethod.data) || {}
-        };
+		var tokens = {};
+		_.each($scope.currentMethod.tokenNames, function (p) {
+			tokens[p] = "";
+		});
+
+		$scope.currentConfig = {
+			tokens: tokens,
+			params: queryParams,
+			data: angular.toJson(angular.copy($scope.currentMethod.data), true) || {}
+		};
+
+		// use repeater so that there is no double binding in key,value
+		$scope.repeater = {
+			tokenNames: angular.copy($scope.currentMethod.tokenNames),
+			qs: angular.copy($scope.currentMethod.qs),
+			data: angular.copy($scope.currentMethod.data) || {}
+		};
 	};
 
-	$scope.sendCall = function() {
-        var formattedUrl = formatFilter($scope.currentMethod.url, $scope.currentConfig.tokens);
-        var d = getData();
-        var p = getParams();
-        var httpConfig = {
-            method: $scope.currentMethod.type,
-            url: baseUrl + formattedUrl
-        };
+	$scope.sendCall = function () {
+		var formattedUrl = formatFilter($scope.currentMethod.url, $scope.currentConfig.tokens);
+		var d = getData();
+		var p = getParams();
+		var httpConfig = {
+			method: $scope.currentMethod.type,
+			url: baseUrl + formattedUrl
+		};
 
-        if(d) httpConfig.data = d;
-        if(p) httpConfig.params = p;
+		if (d) httpConfig.data = d;
+		if (p) httpConfig.params = p;
 
-        $http(httpConfig).
-			success(function(data, status, headers, config) {
-                $scope.resultInfo.data = data;
-                $scope.resultInfo.status = status;
+		$http(httpConfig).
+			success(function (data, status, headers, config) {
+				$scope.resultInfo.data = data;
+				$scope.resultInfo.status = status;
 			}).
-			error(function(data, status, headers, config) {
-                $scope.resultInfo.data = data;
-                $scope.resultInfo.status = status;
+			error(function (data, status, headers, config) {
+				$scope.resultInfo.data = data;
+				$scope.resultInfo.status = status;
 			});
 	};
 
-    $scope.navClass = function(api) {
-        return {
-            last: this.$last,
-            active: $scope.currentMethod == api
-        };
-    };
+	$scope.navClass = function (api) {
+		return {
+			last: this.$last,
+			active: $scope.currentMethod == api
+		};
+	};
 
-    function getMatches(string, regex, index) {
-        index || (index = 1); // default to the first capturing group
-        var matches = [];
-        var match;
-        while (match = regex.exec(string)) {
-            matches.push(match[index]);
-        }
-        return matches;
-    }
+	function getMatches(string, regex, index) {
+		index || (index = 1); // default to the first capturing group
+		var matches = [];
+		var match;
+		while (match = regex.exec(string)) {
+			matches.push(match[index]);
+		}
+		return matches;
+	}
 
-    function getData() {
-        // TODO: fix post data
-        var d = angular.fromJson($scope.currentConfig.data);
-        return d;
-    }
+	function getData() {
+		// TODO: fix post data
+		var d = angular.fromJson($scope.currentConfig.data);
+		return d;
+	}
 
-    function getParams() {
-        var p = angular.copy($scope.currentConfig.params);
-        _.each(p, function(v, k) { if(!v) delete p[k]; });
+	function getParams() {
+		var p = angular.copy($scope.currentConfig.params);
+		_.each(p, function (v, k) {
+			if (!v) delete p[k];
+		});
 
-        if(_.isEmpty(p))
-            return null;
+		if (_.isEmpty(p))
+			return null;
 
-        return p;
-    }
+		return p;
+	}
 
-    $scope.$on('$destroy', function() {
-        // clean up
-    });
+	$scope.$on('$destroy', function () {
+		// clean up
+	});
 
-    initialize();
+	initialize();
 });
