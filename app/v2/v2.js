7486d293eb84c28639592609024aa1f00369c776
Josh Iverson <jiverson222@gmail.com>
2018-12-18 14:59:46 -0700

    Update v2.js

--- app/v2/v2.js
+++ app/v2/v2.js
@@ -2,7 +2,7 @@
 
 angular.module('mfApiExampleApp').controller('V2Ctrl', function ($scope, $http, $filter) {
 
-	$scope.baseUrl = 'https://api-pa.mindflash.com';
+	$scope.baseUrl = 'https://api.mindflash.com';
 	var formatFilter = $filter('format');
 	var tokenRegEx = /(\:)(\w+)(\/{0,1})/gi;
 
